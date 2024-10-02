011538e31d04b20ac24646201fb49d5772b6b146
musashifraco <ezequielestudos11@gmail.com>
2024-10-02 20:17:07 -0300

    feat: deixando rota do swagger publica

--- src/main/java/com/example/product_management_api/filter/ApiKeyFilter.java
+++ src/main/java/com/example/product_management_api/filter/ApiKeyFilter.java
@@ -16,10 +16,19 @@ public class ApiKeyFilter extends OncePerRequestFilter {
     @Value("${api.key}")
     private String apiKey;
 
+
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String apiKeyHeader = request.getHeader("API-Key");
+        String requestURI = request.getRequestURI();
+
+
+        if (requestURI.startsWith("/swagger-ui") || requestURI.startsWith("/v3/api-docs")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         if (apiKeyHeader == null || !apiKeyHeader.equals(apiKey)) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "API Key inválida");
