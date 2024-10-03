05d48f0e0631d6e5f28c6d3e40af47bdf14f48a5
musashifraco <ezequielestudos11@gmail.com>
2024-10-02 22:33:49 -0300

    feat: adicionando documentacao Swagger

--- src/main/java/com/example/product_management_api/controller/ProductController.java
+++ src/main/java/com/example/product_management_api/controller/ProductController.java
@@ -3,6 +3,11 @@ package com.example.product_management_api.controller;
 import com.example.product_management_api.entity.Product;
 import com.example.product_management_api.entity.dto.ProductDTO;
 import com.example.product_management_api.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -25,17 +30,42 @@ public class ProductController {
         this.redisTemplate = redisTemplate;
     }
 
+
+    @Operation(summary = "Finds all product",
+            description = "Finds all product",
+            tags = {"Product"},
+            responses = {@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+                    @ApiResponse(description = "Success",
+                            responseCode = "200",
+                            content = {@Content(mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Product.class)))})})
     @GetMapping
     public List<Product> getAllProducts() {
         List<Product> products = productService.findAll();
-        // Armazena todos os produtos em cache
         redisTemplate.opsForValue().set("products", products);
         return products;
     }
 
+
+    @Operation(summary = "Finds a product",
+            description = "Finds a product",
+            tags = {"Product"},
+            responses = {@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+                    @ApiResponse(description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = Product.class))
+                    ),
+                    @ApiResponse(description = "No Content",
+                            responseCode = "204",
+                            content = @Content)})
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Long id) {
-        // Tenta obter o produto do cache
         List<Product> cachedProducts = redisTemplate.opsForValue().get("products");
         if (cachedProducts != null) {
             return cachedProducts.stream()
@@ -45,43 +75,80 @@ public class ProductController {
                     .orElse(ResponseEntity.notFound().build());
         }
 
-        // Se não estiver no cache, busca no serviço
         return productService.findById(id)
                 .map(product -> {
-                    // Armazena o produto no cache
                     cacheProduct(product);
                     return ResponseEntity.ok().body(product);
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Adds a product",
+            description = "Adds a new product",
+            tags = {"Product"},
+            responses = {@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+                    @ApiResponse(description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = Product.class)))
+            })
     @PostMapping
     public ResponseEntity<Product> createProduct(@Valid @RequestBody ProductDTO productDTO) {
         var savedProduct = productService.save(productDTO);
-        // Invalida o cache após criar um novo produto
         redisTemplate.delete("products");
         return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
     }
 
+    @Operation(summary = "Updates a product",
+            description = "Updates a new product",
+            tags = {"Product"},
+            responses = {@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = Product.class)))
+            })
     @PutMapping("/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable Long id, @Valid @RequestBody ProductDTO productDTO) {
         var updatedProduct = productService.updateProduct(id, productDTO);
-        // Invalida o cache após atualizar o produto
         redisTemplate.delete("products");
         return ResponseEntity.ok(updatedProduct);
     }
 
+    @Operation(summary = "Deletes a product",
+            description = "Deletes a new product",
+            tags = {"Product"},
+            responses = {@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content)
+            })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
-        // Invalida o cache após deletar um produto
         redisTemplate.delete("products");
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Finds cache",
+            description = "Finds cache",
+            tags = {"Cache"},
+            responses = {@ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+                    @ApiResponse(description = "Success",
+                            responseCode = "200",
+                            content = @Content
+                    ),
+                    @ApiResponse(description = "No Content",
+                            responseCode = "204",
+                            content = @Content)})
     @GetMapping("/cache-status")
     public ResponseEntity<String> checkCache() {
-        // Verifica se a chave 'products' está presente no Redis
         if (redisTemplate.hasKey("products")) {
             return ResponseEntity.ok("Cache está ativo e contém produtos.");
         } else {
@@ -90,14 +157,12 @@ public class ProductController {
     }
 
     private void cacheProduct(Product product) {
-        // Recupera a lista de produtos do cache ou cria uma nova lista
         List<Product> cachedProducts = redisTemplate.opsForValue().get("products");
         if (cachedProducts != null) {
             cachedProducts.add(product);
         } else {
-            cachedProducts = List.of(product); // Cria uma nova lista se o cache estiver vazio
+            cachedProducts = List.of(product);
         }
-        // Atualiza o cache com a nova lista de produtos
         redisTemplate.opsForValue().set("products", cachedProducts);
     }
 }
